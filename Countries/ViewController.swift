3dfb5e7b9a76b735ff33796939f858b74b51467d
Alex Drozhak <alex.drozhak@me.com>
2017-08-16 21:54:02 +0300

    Initial Commit

---
+++ Countries/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Countries
+//
+//  Created by Alex Drozhak on 8/16/17.
+//  Copyright © 2017 93Hz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
